396a78a2c08493629a130e95b40830e8098861a6
Yana Naydenova <yana.naydenova00@gmail.com>
2025-09-16 01:13:53 +0300

    Added more animations

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,29 +12,17 @@ export const HeroSection = () => {
     return (
          
         <section id="hero" className="relative min-h-screen flex   items-center relative   mx-auto  relative">
-           <div className="grid grid-cols-1 md:grid-cols-3 items-center relative max-w-[85%] mx-auto gap-3">
-              <div className="relative h-full"> 
-                <Canvas className="min-h-screen opacity-0   flex  animate-fade-in-delay-4  " camera={{ position: [0, 1, 5], fov: 30 }}>
-                    <ambientLight intensity={0.5} />
-                    <directionalLight position={[5, 5, 5]} />
-        
-                    <OrbitControls />  
-                    <group position-y={-1} >
-                        <Avatar/>  
-                    </group>
-                    <ambientLight intensity ={1}/>
-                </Canvas>
-                </div>
+           <div className="grid grid-cols-1 md:grid-cols-2 items-center relative max-w-[70%] mx-auto gap-2">
+               
                 <div className="space-y-6">
                   
-
-                    <h1 className="text-4xl md:text-7xl font-bold tracking-tight text-left">
+                    <h1 className="text-4xl md:text-6xl font-bold tracking-tight text-left">
                         <span className="opacity-0 animate-fade-in">Hi, I'm</span> 
                         <span className="text-primary opacity-0 animate-fade-in-delay-1"> Yana</span> 
                         <span className="text-primary opacity-0 animate-fade-in-delay-2"> Naydenova</span> 
                     </h1>
 
-                <p className="text-lg md:text-xl text-muted-foreground max-w-2xl text-left mx-auto opacity-0 animate-fade-in-delay-2">
+                <p className="text-lg md:text-2xl text-muted-foreground max-w-3xl text-left mx-auto opacity-0 animate-fade-in-delay-2">
                     Computer Science graduate and software developer passionate about creating clean, user-friendly applications and exploring modern technologies.
                 </p>
 
@@ -45,13 +33,26 @@ export const HeroSection = () => {
             </div>
             </div>
 
-            <div className="flex justify-center items-center">
+            {/* <div className="flex justify-center items-center">
                 <img 
                     src={'./public/projects/photo_.jpg'} 
                     alt="My Image" 
                     className="rounded-full w-82 h-82 object-cover opacity-0 animate-fade-in-delay-4" 
                 />
-            </div>
+            </div> */}
+
+            <div className="relative h-full"> 
+                <Canvas className="min-h-screen opacity-0 space-y-6  flex  animate-fade-in-delay-4  " camera={{ position: [0, 2, 5], fov: 30 }}>
+                    <ambientLight intensity={0.5} />
+                    <directionalLight position={[5, 5, 5]} />
+        
+                    <OrbitControls />  
+                    <group position-y={-1} >
+                        <Avatar/>  
+                    </group>
+                    <ambientLight intensity ={1}/>
+                </Canvas>
+                </div>
             </div>
 
             <div  className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col item-center animate-bounce">
